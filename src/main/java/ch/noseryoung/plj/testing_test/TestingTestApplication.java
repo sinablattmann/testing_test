f50de9aa5fdffe8d52740b3164caa1b9545d885d
Sina <sina.blattmann@nyp.ch>
2019-09-06 10:21:16 +0200

    initial commit

---
+++ src/main/java/ch/noseryoung/plj/testing_test/TestingTestApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.plj.testing_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingTestApplication.class, args);
+	}
+
+}
