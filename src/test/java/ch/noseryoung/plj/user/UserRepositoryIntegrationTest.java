f857619caab619ac66fc6c91c0744af40947b7df
Sina <sina.blattmann@nyp.ch>
2019-09-12 11:07:11 +0200

    added h2 db

--- src/test/java/ch/noseryoung/plj/user/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/noseryoung/plj/user/UserRepositoryIntegrationTest.java
@@ -7,12 +7,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class UserRepositoryIntegrationTest {
   @Autowired
   private TestEntityManager entityManager;
